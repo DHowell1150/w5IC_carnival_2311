ca6d5c6b96f7568a472cc6da5cd6b5b577be1ac3
Dana Howell <howelld115@gmail.com>
2024-01-09 11:34:24 -0700

    I2 minus the spending_money error

--- spec/ride_spec.rb
+++ spec/ride_spec.rb
@@ -30,7 +30,7 @@ RSpec.describe Ride do
   end
 
   describe '#board_rider' do
-    it '#board_rider based on visitor.attributes matching ride' do
+    it 'if visitor.attributes matching ride, #ride_log reflects boarding' do
       @visitor1.add_preference(:gentle)
       @visitor2.add_preference(:gentle)
 
@@ -39,15 +39,29 @@ RSpec.describe Ride do
       @ride1.board_rider(@visitor1)
 
       expect(@ride1.rider_log).to eq({@visitor1 => 2, @visitor2 => 1})
+
+      @visitor2.add_preference(:thrilling)
+      @visitor3.add_preference(:thrilling)
+
+      @ride3.board_rider(@visitor1)
+      @ride3.board_rider(@visitor2)
+      @ride3.board_rider(@visitor3)
+
+      expect(@ride3.rider_log).to eq({@visitor3 => 1})
+ 
     end
 
     it "adds to #total_revenue" do
       @ride1.board_rider(@visitor1)
       @ride1.board_rider(@visitor2)
       @ride1.board_rider(@visitor1)
-      
-      
       expect(@ride1.total_revenue).to eq(3)
+
+      @ride3.board_rider(@visitor1)
+      @ride3.board_rider(@visitor2)
+      @ride3.board_rider(@visitor3)
+      expect(@ride3.total_revenue).to eq(6)
+
     end
     
     it 'deducts admission_fee from visitor.spending_money' do
@@ -57,24 +71,14 @@ RSpec.describe Ride do
       
       expect(@visitor1.spending_money).to eq(8)
       expect(@visitor2.spending_money).to eq(4)
+
+      @ride3.board_rider(@visitor1)
+      @ride3.board_rider(@visitor2)
+      @ride3.board_rider(@visitor3)
+
+      expect(@visitor1.spending_money).to eq(8)
+      expect(@visitor2.spending_money).to eq(4)
+      expect(@visitor3.spending_money).to eq(13)
     end
   end
 end
-
-# expect(@visitor2.add_preference(:thrilling)).to eq([:gentle, :thrilling])
-
-# expect(@visitor3.add_preference(:thrilling)).to eq(:thrilling)
-# #=> [:thr])
-
-# @ride3.board_rider(@visitor1)
-# @ride3.board_rider(@visitor2)
-# @ride3.board_rider(@visitor3)
-
-# expect(@visitor1.spending_money).to eq(8)
-# expect(@visitor2.spending_money).to eq(4)
-# expect(@visitor3.spending_money).to eq(13)
-
-# expect(@ride3.rider_log).to eq()
-# #=> {#<@Visitor:0x00000852a0 @height=64, @name="Penny", @preferences=[:thrilling], )@spending_money=13> => 1}
-
-# expect(@ride3.total_revenue).to eq(2)
